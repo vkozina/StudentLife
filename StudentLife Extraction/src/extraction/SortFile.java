f139ac5c456e2f472ac52e95edbfa3b2674dd5c5
Valentina <vkozina1331@gmail.com>
2015-08-05 01:30:09 -0400

    Made sorting static and determined by specified column

--- StudentLife Extraction/src/extraction/SortFile.java
+++ StudentLife Extraction/src/extraction/SortFile.java
@@ -1,9 +1,10 @@
 package extraction;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -13,24 +14,40 @@ import org.apache.poi.ss.usermodel.Row;
 import com.csvreader.CsvReader;
 
 public class SortFile {
-	private String writeFile;
-	private HSSFWorkbook writeTo;
-	private FileOutputStream out;
-
-	public SortFile(File readFile) throws IOException {
-		String writeName = readFile.getName().replace(".csv", ".xls");
-		this.writeFile = readFile.getAbsolutePath().replace(readFile.getName(), "\\sorted\\" + writeName);
-		writeTo = new HSSFWorkbook();
+	
+	//will sort by the specified column (needs to contain number data)
+	public static void sortByCol(File readFile, File writeFile, int col) throws IOException {
+		HSSFWorkbook writeTo = new HSSFWorkbook();
 		writeTo.createSheet("Features");
-		out = new FileOutputStream(new File(writeFile));
-		readLine(readFile);
+		FileOutputStream out = new FileOutputStream(writeFile);
+		
+		CsvReader readFrom =  new CsvReader(readFile.getAbsolutePath());
+		readFrom.readHeaders();
+		writeLine(writeTo, readFrom.getHeaders(), 0);
+		String[] values = new String[1];
+
+		ArrayList<Long> vals = new ArrayList<Long>();
+		while(values.length > 0) {
+			vals.add(Long.parseLong(values[col]));
+		}
+		
+		Collections.sort(vals);
+		
+		while(values.length > 0) {
+			readFrom.readRecord();
+			values = readFrom.getValues();
+			if(values.length > 2 && values[3] != "") {
+				long val = Long.parseLong(values[col]);
+				writeLine(writeTo, values, vals.indexOf(val));		//first row for headers
+			}
+		}
+		writeTo.write(out);
+		out.close();
+		
 	}
 	
-	public String getWriteLoc() {
-		return writeFile;
-	}
-	
-	public void readLine(File readFile) throws IOException {
+	/* sorts directly by id, which is already in order. Only works for call_logs
+	public void sort(File readFile, int col) throws IOException {
 		CsvReader readFrom =  new CsvReader(readFile.getAbsolutePath());
 		readFrom.readHeaders();
 		writeLine(readFrom.getHeaders(), 0);
@@ -48,8 +65,9 @@ public class SortFile {
 		writeTo.write(out);
 		out.close();
 	}
+	*/
 	
-	public void writeLine(String[] rowToPrint, int toRow) throws IOException {
+	public static void writeLine(HSSFWorkbook writeTo, String[] rowToPrint, int toRow) throws IOException {
 		HSSFSheet sheet = writeTo.getSheetAt(0);
 		if(rowToPrint != null) {	//if there is something to print
 			Row row = sheet.createRow(toRow);
